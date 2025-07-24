a4ba2679171866733cd18d63f65d1533280bf641
malindasuhash <malinda.suhash@gmail.com>
2025-07-24 08:03:36 +0100

    Customer manager updated

--- Service/CustomerManager.cs
+++ Service/CustomerManager.cs
@@ -1,12 +1,5 @@
 ﻿using Models;
-using Models.Contract;
 using Models.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service
 {
@@ -44,58 +37,7 @@ namespace Service
         {
             _changeHandler.Manage(customer);
 
-            EventAggregator.Log("Customer updated:'{0}'", customer.ToString());
-        }
-
-        public void SubmitForEvaluation(string id)
-        {
-            _changeHandler.Submit(id, EntityName.Customer);
-        }
-
-        public void ViewDatabase()
-        {
-            var customers = Database.Instance.CustomerCollection;
-
-            EventAggregator.Log("Customers");
-            EventAggregator.Log("---------");
-            foreach (var customer in customers)
-            {
-                EventAggregator.Log("<...RECORD START....>");
-                EventAggregator.Log("Id:{0}", customer.Id);
-                EventAggregator.Log("ClientCopy:'{0}'", customer.ClientCopy == null ? string.Empty : customer.ClientCopy.ToString());
-                EventAggregator.Log("SubmittedCopy:'{0}'", customer.LastestSubmittedCopy == null ? string.Empty : $"Email:{customer.LastestSubmittedCopy.EmailAddress}, State:'{customer.LastestSubmittedCopy.State}'");
-                EventAggregator.Log("WorkingCopy:'{0}'", customer.WorkingCopy == null ? string.Empty : DoFormat(customer.WorkingCopy));
-                EventAggregator.Log("ReadyCopy:'{0}'", customer.ReadyCopy == null ? string.Empty : $"Email:{customer.ReadyCopy.EmailAddress}, State:'{customer.ReadyCopy.State}'");
-                EventAggregator.Log("<...RECORD END....>");
-            }
-
-            var leglEntities = Database.Instance.LegalEntityCollection;
-
-            EventAggregator.Log("Legal Entities");
-            EventAggregator.Log("--------------");
-            foreach (var legalEntity in leglEntities)
-            {
-                EventAggregator.Log("<...RECORD START....>");
-                EventAggregator.Log("Id:{0}", legalEntity.Id);
-                EventAggregator.Log("ClientCopy:'{0}'", legalEntity.ClientCopy == null ? string.Empty : legalEntity.ClientCopy.ToString());
-                EventAggregator.Log("SubmittedCopy:'{0}'", legalEntity.LastestSubmittedCopy == null ? string.Empty : $"Name:'{legalEntity.LastestSubmittedCopy.Name}', State:'{legalEntity.LastestSubmittedCopy.State}'");
-                EventAggregator.Log("WorkingCopy:'{0}'",
-                    legalEntity.WorkingCopy == null || legalEntity.WorkingCopy.Count() == 0 ? "No data" : $"LegalEntity: '{legalEntity.WorkingCopy.First().Name}', State:'{legalEntity.WorkingCopy.First().State}'");
-                EventAggregator.Log("ReadyCopy:'{0}'", legalEntity.ReadyCopy == null ? string.Empty : $"Name:'{legalEntity.ReadyCopy.Name}', State:'{legalEntity.ReadyCopy.State}'");
-                EventAggregator.Log("<...RECORD END....>");
-            }
-        }
-
-        private static string DoFormat(IList<Customer?> workingCopy)
-        {
-            StringBuilder sb = new();
-
-            foreach (var customer in workingCopy)
-            {
-                sb.AppendLine(customer.ToString());
-            }
-
-            return sb.ToString();
+            EventAggregator.Log("Customer updated:'{0}'", customer.Id);
         }
 
         public LegalEntity AddLegalEntity(string customerId, LegalEntity legalEntity)
@@ -110,17 +52,55 @@ namespace Service
             return legalEntity;
         }
 
-        public EntityLayout<LegalEntity, LegalEntityClient> GetLegalEntity(int legalEntityIndex)
+        public Document<LegalEntity> GetLegalEntity(int legalEntityIndex)
         {
-            var layout = Database.Instance.LegalEntityCollection.ElementAt(legalEntityIndex);
+            var legalEntityDocument = Database.Instance.LegalEntityDocuments.ElementAt(legalEntityIndex);
 
-            return layout;
+            return legalEntityDocument;
         }
 
-        public void UpdateLegalEntity(LegalEntityClient legalEntity)
+        public void UpdateLegalEntity(Document<LegalEntity> legalEntity)
         {
             _changeHandler.Manage(legalEntity);
             EventAggregator.Log("LegalEntity updated:'{0}'", legalEntity);
+        }
+
+        public void SubmitForEvaluation(string id)
+        {
+            _changeHandler.Submit(id, EntityName.Customer);
+        }
+
+        public void ViewDatabase()
+        {
+            var customers = Database.Instance.CustomerDocuments;
+
+            EventAggregator.Log("Customers");
+            EventAggregator.Log("---------");
+            foreach (var customer in customers)
+            {
+                EventAggregator.Log("<...RECORD START....>");
+                EventAggregator.Log("Id:{0}", customer.Id);
+                EventAggregator.Log("Draft:'{0}'", customer.Draft == null ? "empty" : customer.Draft);
+                EventAggregator.Log("Submitted:'{0}'", customer.Submitted == null ? "empty" : customer.Submitted);
+                EventAggregator.Log("Approved:'{0}'", customer.Approved == null ? "empty" : customer.Approved);
+                EventAggregator.Log("CurrentState:'{0}'", customer.CurrentState);
+                EventAggregator.Log("<...RECORD END....>");
+            }
+
+            var leglEntities = Database.Instance.LegalEntityDocuments;
+
+            EventAggregator.Log("Legal Entities");
+            EventAggregator.Log("--------------");
+            foreach (var legalEntity in leglEntities)
+            {
+                EventAggregator.Log("<...RECORD START....>");
+                EventAggregator.Log("Id:{0}", legalEntity.Id);
+                EventAggregator.Log("Draft:'{0}'", legalEntity.Draft == null ? "empty" : legalEntity.Draft);
+                EventAggregator.Log("Submitted:'{0}'", legalEntity.Submitted == null ? "empty" : legalEntity.Submitted);
+                EventAggregator.Log("Approved:'{0}'", legalEntity.Approved == null ? "empty" : legalEntity.Approved);
+                EventAggregator.Log("CurrentState:'{0}'", legalEntity.CurrentState);
+                EventAggregator.Log("<...RECORD END....>");
+            }
         }
     }
 }
