49e0a6934bbc237a78e9b6f4093e9c8a27af9f69
malindasuhash <malinda.suhash@gmail.com>
2025-08-04 08:31:06 +0100

    thinking about how to implement state manager.

--- Models/Workflows/Handlers/EvaluationRequireDependencyHandler.cs
+++ Models/Workflows/Handlers/EvaluationRequireDependencyHandler.cs
@@ -10,27 +10,15 @@ namespace Models.Workflows.Handlers
         {
             var eventInfo = (EvaluationRequireDependency)evaluationRequireDependency;
 
-            switch (eventInfo.EntityName)
+            switch (eventInfo.TargetDependencyName)
             {
-                case EntityName.LegalEntity:
-                    var legalEntityDocument = Database.Instance.LegalEntityDocuments.First(l => l.Id == eventInfo.EntityId);
+                case EntityName.Customer:
+                    // Handle the case where the target dependency is a Customer
+                    var customerDocument = Database.Instance.CustomerDocuments.First(c => c.Id == eventInfo.TargetDependencyId);
 
-                    // Transition the legal entity document to the next state
-                    DocumentStateManager.Instance.Transition(legalEntityDocument);
-
-                    // This is where things going to get interesting.
-                    switch (eventInfo.TargetDependencyName)
+                    if (customerDocument.CurrentState == State.Evaluating)
                     {
-                        case EntityName.Customer:
-                            // Handle the case where the target dependency is a Customer
-                            var customerDocument = Database.Instance.CustomerDocuments.First(c => c.Id == eventInfo.TargetDependencyId);
-
-                            if (customerDocument.Submitted != null)
-                            {
-                                // There is an ongoing evaluation for the customer. No action needed.
-                            }
-
-                            break;
+                        // There is an ongoing evaluation for the customer. No action needed.
                     }
 
                     break;
